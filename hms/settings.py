3b094152c5af5cf7f190c3ad3c098e1ecc601c37
aqalyani04 <77406245+aqalyani04@users.noreply.github.com>
2025-05-12 00:29:58 +0500

    First Commit
    Hi

--- hms/settings.py
+++ hms/settings.py
@@ -42,7 +42,6 @@ AUTH_USER_MODEL = 'core.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',  # Required for login
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,7 +120,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'core/static']
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Media files (Uploaded files like PDFs)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
